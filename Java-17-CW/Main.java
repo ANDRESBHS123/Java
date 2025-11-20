8c37e797e9e305f745a0868f8e10420bda66e6fa
ANDRESBHS123 <andresh2789@nycstudents.net>
2025-11-20 17:29:52 +0000

    (11/20/2025)

--- Java-17-CW/Main.java
+++ Java-17-CW/Main.java
@@ -25,7 +25,17 @@ class Main {
   Write a function swapCharacter() that accepts a string to modify, a string(one character) to find, and a string(one character) to replace. The function returns a string with the 'find' strings replaced by the 'replace' string.
   Example: swapCharacter("the seen","e","%")  ==> "th% s%%n"
   */
-  String swapCharacter(String r)
+    String swapCharacter(String s, String find, String replace){
+    String build = "";
+
+    for(int x=0; x<=s.length()-1; x++){
+      if(s.substring(x,x+1).equals(find))
+          build += replace;
+      else
+          build += s.substring(x,x+1);
+    }
+    return build;
+  }
 
 
   /*
@@ -38,7 +48,21 @@ class Main {
   encryption2("abcdefghi")==> "aebfcgdhi"
    process:  "abcdefghi" -> "abcd" "efghi"  -> "aebfcgdhi"
   */
+String encryption2(String s){
+    String build="";
+    int len = s.length();
+    String fHalf = s.substring(0,len/2);
+    String sHalf = s.substring(len/2);
 
+    for(int x=0; x<=fHalf.length()-1; x++)
+        build += fHalf.substring(x, x+1) + sHalf.substring(x, x+1);
+
+    //if string is of odd length, add the last letter of second half-string
+    if(len%2==1)
+        build += sHalf.substring(sHalf.length()-1);
+    
+    return build;
+  }
   
   /*
   Problem 3:
@@ -48,6 +72,24 @@ class Main {
   encryption3("abcde")==> "aebdc"
   */
   
+  String encryption3(String s){
+    String build="";
+    int len=s.length();
+    String fHalf = s.substring(0,len/2);
+    String sHalf = s.substring(len/2);
+    String sHalfRvrs = reverse(sHalf);
 
+    for(int x=0; x<=fHalf.length()-1;x++){
+      build += fHalf.substring(x,x+1) + sHalfRvrs.substring(x,x+1);
+    }
+    //if string is of odd length, add the last letter of reversed second half-string
+    if(len%2==1)
+      build += sHalfRvrs.substring(sHalfRvrs.length()-1);
+    
+    return build;
+  }
+
+  
+}
   
 }
