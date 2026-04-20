55699116229df14ae50375272eeb2ea6e1c6e97d
ANDRESBHS123 <andresh2789@nycstudents.net>
2026-04-20 16:00:55 +0000

    (3-20-26)

---
+++ Java-50-CW/script.js
@@ -0,0 +1,35 @@
+let employees, info;
+
+async function init(){
+  // JSON data retrieval process  
+  let link = "https://shiny-space-yodel-q74qrx5qxwrpfx5gr-8500.app.github.dev/"; //replace with your specific link (Make it public)
+  // The URL above must be running in a separate broswer tab. (Alternate use: http://localhost:8500 )
+  let route= "/employees";
+
+  info = await fetch(link+route);
+  employees = await info.json();
+
+  console.log(employees); // confirm data retrieval
+
+  // call the function (from Problem 1) to generate the cards
+  generateCards(employees);
+}
+
+// Problem 1: Create a function that builds a card for each employee using the div with a class of 'card'.
+// Each card should contain the employee ID, first name, last name, and title.
+// Make the main output div with id of 'output' be a flex container using CSS. 
+function generateCards(employees){
+
+  let output = "";
+  let build = "";
+  
+  for(let i=0; i<employees.length; i++){
+    let employee = employees[i];
+    build += `<div `
+
+
+  // Problem 2: Add CSS Styling to style the info cards.
+    
+
+
+}
